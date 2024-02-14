7b2e5944a38d3405e2c66a953d43b7d8009b9994
Drixner <drixnerrafael1@gmail.com>
2024-02-14 02:00:14 -0500

    Add PokemonPage component and import necessary files

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,12 @@
+import { default } from '../../App.vue';
+<template>
+  <h1>Quien ese ese pokemo?</h1>
+</template>
+
+<script>
+export default {
+  name: "PokemonPage",
+};
+</script>
+
+<style></style>
