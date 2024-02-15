9f13658e1a3a273c7ff78d29d886ac4d0e81284e
Drixner <drixnerrafael1@gmail.com>
2024-02-15 00:50:09 -0500

    Update PokemonPicture.vue component styling and add fade-in effect

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -1,5 +1,15 @@
 <template>
-  <h3>Pokemon Picture</h3>
+  .componet
+  <img
+    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+    class="hidden-pokemon"
+    alt="Pokemon"
+  />
+  <img
+    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+    class="fade-in"
+    alt="Pokemon"
+  />
 </template>
 
 <script>
@@ -8,4 +18,43 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.pokemon-container {
+  height: 200px;
+}
+img {
+  height: 200px;
+  position: absolute;
+  right: 32%;
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+}
+.hidden-pokemon {
+  filter: brightness(0);
+}
+
+/* Pokemon Options */
+ul {
+  list-style-type: none;
+}
+li {
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+  margin-bottom: 10px;
+  width: 250px;
+}
+
+li:hover {
+  background-color: rgba(0, 0, 0, 0.05);
+}
+
+.options-container {
+  display: flex;
+  justify-content: center;
+}
+</style>
