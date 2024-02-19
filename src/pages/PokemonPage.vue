529bfd20c3b634b9ad54bb7810e4653ff29a4f89
Drixner <drixnerrafael1@gmail.com>
2024-02-18 22:44:11 -0500

    Refactor getPokemonOptions to use async/await

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -2,7 +2,7 @@ import { default } from '../../App.vue';
 <template>
   <h1>Quien es ese pokemon?</h1>
   <PokemonPicture :pokemonId="180" :showPokemon="true" />
-  <PokemonOptions />
+  <PokemonOptions :pokemon="pokemomArr" />
 </template>
 
 <script>
@@ -10,13 +10,27 @@ import PokemonPicture from "@/components/PokemonPicture";
 import PokemonOptions from "@/components/PokemonOptions";
 import getPokemonOptions from "@/helpers/getPokemonOptions";
 
-console.log(getPokemonOptions());
+// console.log(getPokemonOptions());
 
 export default {
   name: "PokemonPage",
   components: {
     PokemonPicture,
     PokemonOptions,
+  },
+  data() {
+    return {
+      pokemomArr: [],
+    };
+  },
+  methods: {
+    async mixPokemonArray() {
+      this.pokemomArr = await getPokemonOptions();
+      console.log(this.pokemomArr);
+    },
+  },
+  mounted() {
+    this.mixPokemonArray();
   },
 };
 </script>
