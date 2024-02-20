64d7fe3227c1815ef9b3829fe4688784ab0e930f
Drixner <drixnerrafael1@gmail.com>
2024-02-20 12:03:44 -0500

    Refactor PokemonPage.vue component

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -3,8 +3,15 @@
 
   <div v-else>
     <h1>Quien es ese pokemon?</h1>
+
     <PokemonPicture :pokemonId="pokemon.id" :showPokemon="showPokemon" />
-    <PokemonOptions :pokemons="pokemonArr" @selection="checkAnswer(1, 2)" />
+
+    <PokemonOptions :pokemons="pokemonArr" @selection="checkAnswer" />
+
+    <div v-if="showAnswer">
+      <h2 class="fade-in">{{ message }}</h2>
+      <button @click="newGame">Nuevo juego</button>
+    </div>
   </div>
 </template>
 
@@ -26,6 +33,8 @@ export default {
       pokemonArr: [],
       pokemon: null,
       showPokemon: false,
+      showAnswer: false,
+      message: "",
     };
   },
   methods: {
@@ -34,8 +43,21 @@ export default {
       const rndInt = Math.floor(Math.random() * 4);
       this.pokemon = this.pokemonArr[rndInt];
     },
-    checkAnswer() {
-      console.log("pokemon page llamado");
+    checkAnswer(pokemonId) {
+      this.showPokemon = true;
+      this.showAnswer = true;
+      if (pokemonId === this.pokemon.id) {
+        this.message = `Correcto! ${this.pokemon.name}`;
+      } else {
+        this.message = `Incorrecto! era ${this.pokemon.name}`;
+      }
+    },
+    newGame() {
+      this.showPokemon = false;
+      this.showAnswer = false;
+      this.pokemonArr = [];
+      this.pokemon = null;
+      this.mixPokemonArray();
     },
   },
   mounted() {
