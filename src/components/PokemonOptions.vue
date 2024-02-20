7eb34159ee403a6ac6a8cc90913b65c6e91971ef
Drixner <drixnerrafael1@gmail.com>
2024-02-19 21:56:11 -0500

    Refactor PokemonOptions.vue to emit selection event on click

--- src/components/PokemonOptions.vue
+++ src/components/PokemonOptions.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="options-container">
     <ul>
-      <li v-for="pokemon in pokemons" :key="pokemon.id">{{ pokemon.name }}</li>
+      <li
+        v-for="pokemon in pokemons"
+        :key="pokemon.id"
+        @click="$emit('selection')"
+      >
+        {{ pokemon.name }}
+      </li>
     </ul>
   </div>
 </template>
-#
 
 <script>
 export default {
